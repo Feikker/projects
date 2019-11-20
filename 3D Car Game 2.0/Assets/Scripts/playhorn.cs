38627296fbb3ce66897a01d6cdff6db224d79c75
Feikker <jari.pimiaa@hotmail.com>
2019-11-20 05:38:18 +0200

    ACG

---
+++ 3D Car Game 2.0/Assets/Scripts/playhorn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playhorn : MonoBehaviour
+{
+
+    AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public void beep()
+    {
+        audio.Play();
+    }
+}
---
+++ 3D Car Game 2.0/Assets/Scripts/scenecontrol.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenecontrol : MonoBehaviour
+{
+
+    public void changelevel(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+}
